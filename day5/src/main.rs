db8d344c9ef6ef57f0fcfc44b092b03e706dc76f
AG Stephan <godtheresnonamesleft@gmail.com>
2018-12-13 23:00:05 -0600

    day 5 part 1

---
+++ day5/src/main.rs
@@ -0,0 +1,71 @@
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::time::Instant;
+
+fn react(polymer: String) -> String {
+    let mut output_chars : Vec<char> = polymer
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect();
+    
+    loop {
+        let mut reduced = Vec::new();
+
+        let mut i = 1;
+        while i < output_chars.len() {
+            if output_chars[i - 1].is_ascii_uppercase() {
+                if output_chars[i - 1].to_ascii_lowercase() == output_chars[i] {
+                    i += 2;
+                    continue;
+                } else {
+                    reduced.push(output_chars[i - 1]);
+                    i += 1;
+                }
+            } else {
+                if output_chars[i - 1].to_ascii_uppercase() == output_chars[i] {
+                    i += 2;
+                    continue;
+                } else {
+                    reduced.push(output_chars[i - 1]);
+                    i += 1;
+                }
+            }
+        }
+
+        reduced.push(output_chars[output_chars.len() - 1]);
+
+        if reduced.len() == output_chars.len() {
+            break;
+        }
+
+        output_chars = reduced;
+    }
+
+    return output_chars.iter().collect();
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let filename = &args[1];
+
+    println!("reading input from \"{}\"...", filename);
+
+    let mut f = File::open(filename)
+        .expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading file");
+    
+    let start = Instant::now();
+
+    let reduced = react(contents);
+    
+    println!("Final element:\n{}", reduced);
+    println!("{} total elements", reduced.len());
+    
+    println!("took {:?}", start.elapsed());
+}
